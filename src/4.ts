62d11b39e7acdc96740bcac4876bf2262e6f3b90
ira089 <gvir2015@gmail.com>
2024-02-28 23:07:22 +0200

    TS-hw-03_ fin1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+  private signature: number;
+
+  getSignature(): number {
+    this.signature = Math.random();
+    console.log(this.signature);
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private keyPerson: Key) {}
+
+  getKey() {
+    return this.keyPerson.getSignature();
+  }
+}
+
+abstract class House {
+  public tenants: Person[] = [];
+  public door: boolean = false;
+  constructor(protected keyHouse: Key) {}
+
+  comeIn(tenant: Person) {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+  public abstract openDoor(key: number): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key): void {
+    if (key === this.keyHouse.getSignature) {
+      this.door = true;
+    }
+    this.door = false;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +46,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
